d4cad99f80e8d279a3fc5300677e6e3ba82cc264
Jesse Smick <janka102@gmail.com>
2014-10-07 17:48:44 -0700

    Added two examples from http://facebook.github.io/react/

---
+++ examples/stateful/server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+    app = express(),
+    swig = require('swig'),
+    port = 3000;
+
+// swig-react-tags
+require('../../index')(swig);
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these set to `false` in production!
+app.set('view cache', false);
+swig.setDefaults({
+    cache: false
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Example started on http://localhost:%d/', port);
+});
